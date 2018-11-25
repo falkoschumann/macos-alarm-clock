9fd1559cad96e2fcb99410aeb693146cf1616d6f
Falko Schumann <falko.schumann@muspellheim.de>
2018-11-25 13:20:18 +0100

    Initial Commit

---
+++ Alarm Clock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Alarm Clock
+//
+//  Created by Falko Schumann on 25.11.18.
+//  Copyright © 2018 Falko Schumann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Alarm Clock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Alarm Clock
+//
+//  Created by Falko Schumann on 25.11.18.
+//  Copyright © 2018 Falko Schumann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
